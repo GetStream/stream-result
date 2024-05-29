61bcfb5fe4692fbc387daae6171dbd8bc329dfa2
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-05-29 12:38:14 +0200

    Bump version

--- buildSrc/src/main/kotlin/io/getstream/Configurations.kt
+++ buildSrc/src/main/kotlin/io/getstream/Configurations.kt
@@ -5,7 +5,7 @@ object Configurations {
     const val targetSdk = 33
     const val minSdk = 21
     const val majorVersion = 1
-    const val minorVersion = 1
+    const val minorVersion = 2
     const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
