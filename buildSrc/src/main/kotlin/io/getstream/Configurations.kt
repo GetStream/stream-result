3b12d80094a6c984c342059f9e0a9a44955b413b
skydoves <skydoves@naver.com>
2024-09-23 13:02:08 +0900

    Prepare for release 1.3.0

--- buildSrc/src/main/kotlin/io/getstream/Configurations.kt
+++ buildSrc/src/main/kotlin/io/getstream/Configurations.kt
@@ -5,7 +5,7 @@ object Configurations {
     const val targetSdk = 35
     const val minSdk = 21
     const val majorVersion = 1
-    const val minorVersion = 2
+    const val minorVersion = 3
     const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
