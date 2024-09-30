8ed70cc7b00d87854b04d81b970549ab5b9ed002
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-09-30 14:36:13 +0200

    Add Tests for RetrofitCall

---
+++ stream-result-call-retrofit/src/test/kotlin/io/getstream/result/call/retrofit/BlockedRetrofit2Call.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.getstream.result.call.retrofit
+
+import io.getstream.result.call.dispatcher.CallDispatcherProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.Request
+import okio.Timeout
+import org.mockito.kotlin.mock
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+import java.util.concurrent.atomic.AtomicBoolean
+
+internal class BlockedRetrofit2Call<T>(
+  private val scope: CoroutineScope,
+  private val value: T? = null,
+  private val error: IOException? = null
+) : retrofit2.Call<T> {
+
+  init {
+    if (value == null) {
+      requireNotNull(error) {
+        "BlockedRetrofit2Call should be initialized with an error or value not null"
+      }
+    }
+    if (error == null) {
+      requireNotNull(value) {
+        "BlockedRetrofit2Call should be initialized with an error or value not null"
+      }
+    }
+    if (error != null && value != null) error("BlockedRetrofit2Call can't be initialized with a value and an error")
+  }
+
+  private val isBlocked = AtomicBoolean(true)
+  private val started = AtomicBoolean(false)
+  private val completed = AtomicBoolean(false)
+  private val cancelled = AtomicBoolean(false)
+
+  fun unblock() {
+    isBlocked.set(false)
+  }
+
+  private suspend fun run() = withContext(CallDispatcherProvider.IO) {
+    started.set(true)
+    while (isBlocked.get()) {
+      delay(10)
+    }
+    if (!cancelled.get()) completed.set(true)
+  }
+
+  fun isStarted(): Boolean = started.get()
+  fun isCompleted(): Boolean = completed.get()
+  override fun enqueue(callback: Callback<T>) {
+    scope.launch {
+      run()
+      if (value != null) callback.onResponse(this@BlockedRetrofit2Call, Response.success(value))
+      if (error != null) callback.onFailure(this@BlockedRetrofit2Call, error)
+    }
+  }
+
+  override fun execute(): Response<T> = runBlocking {
+    run()
+    if (value != null) {
+      Response.success(value)
+    } else {
+      throw error!!
+    }
+  }
+
+  override fun isExecuted(): Boolean = started.get()
+  override fun cancel() { cancelled.set(true) }
+  override fun isCanceled(): Boolean = cancelled.get()
+  override fun request(): Request = mock()
+  override fun timeout(): Timeout = mock()
+  override fun clone(): Call<T> = BlockedRetrofit2Call(scope, value, error)
+}
